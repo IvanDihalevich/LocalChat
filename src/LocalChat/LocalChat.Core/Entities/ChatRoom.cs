c836f2c3a44bc18428e31148c8a5e5b0eb21a8d7
lixoO0 <153216405+lixoO0@users.noreply.github.com>
2024-04-08 00:01:56 +0300

    add new entiti Chat Room
     and update class user,update referenses

--- src/LocalChat/LocalChat.Core/Entities/ChatRoom.cs
+++ src/LocalChat/LocalChat.Core/Entities/ChatRoom.cs
@@ -6,12 +6,11 @@ using System.Threading.Tasks;
 
 namespace LocalChat.Core.Entities
 {
-    public class User: IEntity<Guid>
+    public class ChatRoom
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public List<ChatRoom> ChatRooms { get; set; }
+        public List<User> Users { get; set; }
+        public List<Message> Messages { get; set; }
     }
 }
