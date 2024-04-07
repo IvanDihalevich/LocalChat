2df0eb7e222e60218337327d2cee940b2765cec8
Ivan Dihalevych <143941559+IvanDihalevich@users.noreply.github.com>
2024-04-07 23:53:01 +0300

    update

--- src/LocalChat/LocalChat.Core/Entities/User.cs
+++ src/LocalChat/LocalChat.Core/Entities/User.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace LocalChat.Core.Entities
 {
-    internal class User
+    public class User: IEntity<Guid>
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        
     }
 }
