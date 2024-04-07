f779ef361c04884c330e9ecec93b7eb5fd03808f
Ivan Dihalevych <143941559+IvanDihalevich@users.noreply.github.com>
2024-04-07 23:41:17 +0300

    update

--- src/LocalChat/LocalChat.Core/Entities/Message.cs
+++ src/LocalChat/LocalChat.Core/Entities/Message.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace LocalChat.Core.Entities
 {
-    internal class Massage
+    public class Message: IEntity<Guid>
     {
+        public Guid Id { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+        public string Text { get; set; }
+        public DateTime SendTime { get; set; }
     }
 }
