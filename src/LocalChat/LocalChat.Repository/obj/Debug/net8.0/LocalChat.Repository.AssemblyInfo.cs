cd5b4ced2261f6e29b28f394e2ce0d761dfdb51f
Ivan Dihalevych <143941559+IvanDihalevich@users.noreply.github.com>
2024-04-23 23:50:22 +0300

    remake view

--- src/LocalChat/LocalChat.Repository/obj/Debug/net8.0/LocalChat.Repository.AssemblyInfo.cs
+++ src/LocalChat/LocalChat.Repository/obj/Debug/net8.0/LocalChat.Repository.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LocalChat.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6c23fc723c6ce12e0c4534ff6f42b596a5c1a3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+387b3cbd7c6a852f7c03464769150c8a7a14cca6")]
 [assembly: System.Reflection.AssemblyProductAttribute("LocalChat.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LocalChat.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/LocalChat/LocalChat.UI/Program.cs
+++ src/LocalChat/LocalChat.UI/Program.cs
@@ -44,7 +44,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=ChatRoom}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
